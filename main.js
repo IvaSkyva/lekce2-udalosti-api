f5960c2cf6e3d19a76dd6dad4e24c025ca8d768a
Ivana Skýva <ivana.rybakova@gmail.com>
2021-03-17 01:14:56 +0100

    ukol z lekce

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // tady je místo pro náš program
+function cerveny() {
+    let napis = document.querySelector('.textlev');
+    napis.style.color = 'red';
+}
+
+function tucne() {
+    let napis = document.querySelector('.textlev');
+    napis.style.fontWeight = 'bold';
+}
