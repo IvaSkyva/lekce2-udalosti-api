c8dfc1c3bfe95d6eebc8b29baece488f90105d59
Ivana Skýva <ivana.rybakova@gmail.com>
2021-03-18 22:20:06 +0100

    domaci ukol 3

--- main.js
+++ main.js
@@ -15,3 +15,34 @@ function vetsi() {
     document.querySelector('.textlev').style.fontSize = newfontSize +'px';
     fontSize=newfontSize; 
 }
+
+function prehraj() {    
+    let audio = document.querySelector('.zvuk');
+    audio.play();
+}
+
+function prerus() {    
+    let audio = document.querySelector('.zvuk');
+    audio.pause();
+}
+
+
+function ztisit() {    
+    let audio = document.querySelector('.zvuk');
+    audio.volume = 0;
+}
+
+function hlasitost() {    
+    let audio = document.querySelector('.zvuk');
+    audio.volume = 0.5;
+}
+
+function zesilit() {    
+    let audio = document.querySelector('.zvuk');
+    audio.volume = 1;
+}
+
+function nazacatek() {    
+    let audio = document.querySelector('.zvuk');
+    audio.currentTime = 0;
+}
